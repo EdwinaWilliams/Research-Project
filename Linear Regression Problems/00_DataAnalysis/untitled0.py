0a43412c85137ef96b467b76421a5d24448c335c
edwinagw <egwilliams1991@gmail.com>
2019-06-20 02:29:44 +0200

    WIP Linear regression plot

--- Linear Regression Problems/00_DataAnalysis/untitled0.py
+++ Linear Regression Problems/00_DataAnalysis/untitled0.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun  4 21:24:19 2019
+Created on Mon Jun  3 23:15:31 2019
 
 @author: egwil
 """
 
-# Import Data
-df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
-df.reset_index(inplace=True)
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 20:38:04 2019
 
-# Prepare data
-df['year'] = [d.year for d in df.date]
-df['month'] = [d.strftime('%b') for d in df.date]
-years = df['year'].unique()
+@author: egwil
+Title: Line plot - connected
+Description: Line plot to visualize the time series 
+Filters: StatName 
+"""
 
-print(df.head())
+"""
+Step 1: Import libraries
+"""
 
-# Prep Colors
-#np.random.seed(100)
-#mycolors = np.random.choice(list(mpl.colors.XKCD_COLORS.keys()), len(years), replace=False)
-#
-## Draw Plot
-#plt.figure(figsize=(16,12), dpi= 80)
-#for i, y in enumerate(years):
-#    if i > 0:        
-#        plt.plot('month', 'value', data=df.loc[df.year==y, :], color=mycolors[i], label=y)
-#        plt.text(df.loc[df.year==y, :].shape[0]-.9, df.loc[df.year==y, 'value'][-1:].values[0], y, fontsize=12, color=mycolors[i])
-#
-## Decoration
-#plt.gca().set(xlim=(-0.3, 11), ylim=(2, 30), ylabel='$Drug Sales$', xlabel='$Month$')
-#plt.yticks(fontsize=12, alpha=.7)
-#plt.title("Seasonal Plot of Drug Sales Time Series", fontsize=20)
-#plt.show()
+#import matplotlib for visualisations
+from dateutil.parser import parse #use parser make the date colum to be parsed as a date field
+import matplotlib as plt
+from mlxtend.plotting import plot_linear_regression 
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+
+"""
+Step 2 --> Set variables
+""" 
+
+filepath = 'C:/Users/egwil/OneDrive - University of Cape Town/UCT/Research/Data/data/ModelTesting.xlsx'
+sheetname = 'Train'
+
+"""
+Step 2 -->Load dataset 
+""" 
+
+df = pd.read_excel(filepath, sheet_name=sheetname)
+
+temp_dataholder = df.index.values.reshape(-1, 1)
+#X_tmp = 
+X = np.array(temp_dataholder[0:6])
+y = np.array(df.iloc[:6, -1:])
+#y = np.array([df.values('MaxTemp (°C)')])
+
+print(X,y)
+
+intercept, slope, corr_coeff = plot_linear_regression(X, y)
+plt.show()
